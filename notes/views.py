206cfd9045db22921937c83adafa5a63579b355f
горбуновв <vadim.212@inbox.ru>
2020-03-13 21:04:22 +0600

    update authorization

--- notes/views.py
+++ notes/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from notes import templates
 from notes.models import Note, User
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.views import LoginView
 from notes.forms import RegisterForm, login, LoginForm
 
 
@@ -13,28 +14,32 @@ def index(request):
     print('session ->', request.session['logged_user_id'])
     return render(request, 'index.html')
 
-
+# LoginView
+# LoginView
+# LoginView
+# check method "login" (django.contrib.auth.__init__.py)
 def sign_in(request):
     # user = User.objects.get()
     # request.session['logged_user_id'] = user.id
     # return render(request, 'signin.html')
-    # if request.method == 'POST':
-    #     form = LoginForm(request.POST)
-    #     if form.is_valid():
-    #         username = form.cleaned_data.get('username')
-    #         password = form.cleaned_data.get('password')
-    #         user = authenticate(username=username, password=password)
-    #         User.objects.get()
-    #         if user is not None:
-    #             if user.is_active:
-    #                 login(request, user)
-    #                 return HttpResponse('Authenticated successfully')
-    #             else:
-    #                 return HttpResponse('Disabled account')
-    #         else:
-    #             return HttpResponse('Invalid login')
-    # else:
-    #     form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            # User.objects.get()
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    # return HttpResponse('Authenticated successfully')
+                    return redirect('index')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
     return render(request, 'account/login.html')#, {'form': form})
 
 
@@ -49,9 +54,9 @@ def sign_up(request):
             raw_password = form.cleaned_data.get('password1')
             # print(raw_password)
             user = authenticate(username=username, password=raw_password)
-            # login(request, user.id)
-            new_user = login(username, email, raw_password)
-            request.session['logged_user_id'] = new_user.id
+            login(request, user)
+            # new_user = login(username, email, raw_password)
+            request.session['logged_user_id'] = user
             # print('--->', new_user, new_user.id, new_user.login, new_user.email, new_user.password)
             return redirect(index)
     else:
